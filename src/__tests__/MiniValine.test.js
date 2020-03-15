22fad38ada588e4921ebb5d40417ea1d6db2409f
MH <616001163@qq.com>
2020-03-15 11:11:30 +0800

    Update MiniValine.test.js

--- src/__tests__/MiniValine.test.js
+++ src/__tests__/MiniValine.test.js
@@ -3,11 +3,9 @@ test('MiniValine', () => {
   document.body.innerHTML = '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"></head><body><div class="comment"></div></body></html>'
   MiniValine({
     el: '.comment',
-    appId: 's1UXuqJzhVJMt6uMz5bKD6qW-gzGzoHsz',
-    appKey: 'TwjpsjDesTnAK4Ot8y6k9wpw',
+    appId: 's1UXuqJzhVJMt6uMz5bKD6qW-gzGzoHszpppkmy',
+    appKey: 'TwjpsjDesTnAK4Ot8y6k9wpwkmvmk',
     placeholder: 'Write a Comment',
-    lang: 'en',
-    pathname: '/guestbook/index.html',
-    adminEmailMd5: 'DE8A7AA53D07E6B6BCEB45C64027763D'
+    lang: 'en'
   })
 })
