e66574b6f07dc3ab3c861b16dddccadbd26fdb7c
MHuiG <616001163@qq.com>
2020-03-20 15:11:47 +0800

    fix: codestyle

--- webpack.config.js
+++ webpack.config.js
@@ -1,71 +1,70 @@
-var webpack = require('webpack');
-var path = require('path');
-var autoprefixer = require('autoprefixer');
+var webpack = require('webpack')
+var path = require('path')
 
-var libraryName = 'MiniValine';
-var env = process.env.WEBPACK_ENV;
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'src');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+var libraryName = 'MiniValine'
+var env = process.env.WEBPACK_ENV
+var ROOT_PATH = path.resolve(__dirname)
+var APP_PATH = path.resolve(ROOT_PATH, 'src')
+var BUILD_PATH = path.resolve(ROOT_PATH, 'dist')
 
-var plugins = [];
+var plugins = []
 if (env !== 'dev') {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true
-        })
-    );
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true
+    })
+  )
 }
 
 module.exports = {
-    entry: './src/index.js',
+  entry: './src/index.js',
 
-    output: {
-        path: BUILD_PATH,
-        publicPath: "/dist/",
-        filename: libraryName + '.min.js',
-        library: libraryName,
-        libraryTarget: 'umd',
-        umdNamedDefine: true
+  output: {
+    path: BUILD_PATH,
+    publicPath: '/dist/',
+    filename: libraryName + '.min.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+
+  devtool: 'source-map',
+
+  devServer: {
+    publicPath: '/dist/',
+    inline: true,
+    port: 8088
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: APP_PATH,
+      options: {
+        presets: ['@babel/preset-env']
+      }
     },
-
-    devtool: 'source-map',
-
-    devServer: {
-        publicPath: "/dist/",
-        inline: true,
-        port: 8088
+    {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'postcss-loader',
+        'sass-loader'
+      ],
+      include: APP_PATH
     },
-
-    module: {
-        rules: [{
-                test: /\.js$/,
-                loader: 'babel-loader',
-                include: APP_PATH,
-                options: {
-                   presets: ['@babel/preset-env']
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    'postcss-loader',
-                    'sass-loader'
-                ],
-                include: APP_PATH
-            },
-            {
-                test: /\.(png|jpg)$/,
-                loader: 'url-loader?limit=40000'
-            },
-			{
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            }
-        ]
+    {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=40000'
     },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }
+    ]
+  },
 
-    plugins: plugins
-};
+  plugins: plugins
+}
