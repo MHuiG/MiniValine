d9cf089df545311a721fe462a1d07c1b12987630
MHuiG <616001163@qq.com>
2020-03-22 09:16:09 +0800

    fix: Expected parentheses around arrow function argument.

--- src/utils/GetIP.js
+++ src/utils/GetIP.js
@@ -4,7 +4,7 @@ const GetIP = () => {
   Utils.create('script', 'src', 'https://api.ip.sb/jsonip?callback=getIP')
   const s = document.getElementsByTagName('script')[0]
   document.head.append(s)
-  window.getIP = json => {
+  window.getIP = (json) => {
     defaultComment.ip = json.ip
   }
 }
