3118d02a7246621b53f146dcc74121a7a85313e4
MHuiG <616001163@qq.com>
2020-04-02 10:41:28 +0800

    Release:1.2.25

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import MiniValineFactory from './MiniValine'
-import(/* webpackChunkName: "style" */'./style/index.scss')
 import { VERSION } from './Default'
+import(/* webpackChunkName: "style" */'./style/index.scss')
 const MiniValine = (option) => {
   console.log('%c MiniValine %c v' + VERSION + ' ', 'color: white; background: #0078E7; padding:5px 0;', 'padding:4px;border:1px solid #0078E7;')
   return new MiniValineFactory(option)
