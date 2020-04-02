a978c4fcceeb82dcd1f569e7b0f85eb2881cf6d1
MHuiG <616001163@qq.com>
2020-04-02 09:27:25 +0800

    ADD VERSION FLAG

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const VERSION = require('../package.json').version;
 import MiniValineFactory from './MiniValine'
 import(/* webpackChunkName: "style" */'./style/index.scss')
 
 const MiniValine = (option) => {
+  console.log("%c MiniValine %c v"+VERSION+" ","color: white; background: #0078E7; padding:5px 0;","padding:4px;border:1px solid #0078E7;")
   return new MiniValineFactory(option)
 }
 
