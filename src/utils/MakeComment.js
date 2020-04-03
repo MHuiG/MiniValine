74e6b71e1187e9260349600ecab5d98b9a763e7a
MHuiG <616001163@qq.com>
2020-04-03 09:14:47 +0800

    xss

--- src/utils/MakeComment.js
+++ src/utils/MakeComment.js
@@ -1,15 +1,15 @@
-import xss from 'xss'
-
 const MakeComment = (root) => {
   var tmp = root.Comment.comment.replace(/!\(:(.*?\.\w+):\)/g, `<img src="${root.emoticonUrl}/$1" alt="$1" class="vemoticon-img">`)
-  tmp = xss(tmp, {
-    onIgnoreTagAttr (tag, name, value, isWhiteAttr) {
-      if (name === 'class') {
-        return `${name}="${xss.escapeAttrValue(value)}"`
+  import(/* webpackChunkName: "xss" */'xss').then(({ xss }) => {
+    tmp = xss(tmp, {
+      onIgnoreTagAttr (tag, name, value, isWhiteAttr) {
+        if (name === 'class') {
+          return `${name}="${xss.escapeAttrValue(value)}"`
+        }
       }
-    }
-  }
-  )
+    })
+  })
+
   return tmp
 }
 module.exports = MakeComment
