c40bec230ce09f2fa0a251e51293af9c3cbd059f
MHuiG <616001163@qq.com>
2020-04-06 10:36:01 +0800

    Update initLibs.js

--- src/utils/initLibs.js
+++ src/utils/initLibs.js
@@ -1,7 +1,11 @@
 const initLibs = (root) => {
-  import(/* webpackChunkName: "lib" */'./plugins/lib.js').then(({ lib }) => {
-    lib()
-    root.initCheck()
-  })
+  var checkie = setInterval(function () {
+    if (typeof Promise == 'undefined') { return }
+    clearInterval(checkie)
+    import(/* webpackChunkName: "lib" */'./plugins/lib.js').then(({ lib }) => {
+      lib()
+      root.initCheck()
+    })
+  }, 5)
 }
 module.exports = initLibs
