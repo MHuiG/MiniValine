999f75a590c846576dc5c196a047785086a5c86e
MHuiG <616001163@qq.com>
2020-04-09 14:35:42 +0800

    .

--- src/utils/script.js
+++ src/utils/script.js
@@ -2,9 +2,9 @@ import { ip } from '../Default'
 import getScript from './plugins/getScript'
 const script = (root) => {
   getScript(ip)
+  getScript('https://cdn.polyfill.io/v3/polyfill.min.js?features=es6')
   if (!!window.ActiveXObject || 'ActiveXObject' in window) {
     root.config.md = false
-    getScript('https://cdn.polyfill.io/v2/polyfill.min.js?features=es6')
   }
 }
 module.exports = script
