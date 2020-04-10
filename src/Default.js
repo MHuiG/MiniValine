78f3a026811a84dbb8d5fca8b343c6c844555ad0
MHuiG <616001163@qq.com>
2020-04-10 09:17:09 +0800

    Release:1.2.40

--- src/Default.js
+++ src/Default.js
@@ -1,4 +1,4 @@
-export const VERSION = '1.2.39'
+export const VERSION = '1.2.40'
 export const GravatarBaseUrl = 'https://gravatar.loli.net/avatar/'
 export const ip = 'https://api.ip.sb/jsonip?callback=getIP'
 export const EmoticonUrl = 'https://cdn.jsdelivr.net/npm/alus'
