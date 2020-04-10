5c340c3a39c694dac74b2953328a37d0f5d3ad2b
MHuiG <616001163@qq.com>
2020-04-10 09:10:18 +0800

    math min

--- src/utils/plugins/initMathJax3.js
+++ src/utils/plugins/initMathJax3.js
@@ -1,21 +1,9 @@
 import getScript from './getScript'
 const init = () => {
   const script = document.createElement('script')
-  script.text = `
-window.MathJax = {
-  tex: {
-    inlineMath: [ ['$','$'] ],
-    displayMath: [ ['$$','$$']]
-  },
-  options: {
-    skipHtmlTags: ['script', 'noscript', 'style', 'textarea', 'pre','code', 'a', 'annotation', 'annotation-xml'],
-    ignoreHtmlClass: 'tex2jax_ignore',
-    processHtmlClass: 'tex2jax_process'
-  },
-};
-`
+  script.text = `window.MathJax = {tex: {inlineMath: [ ['$','$'] ]}}`
   document.getElementsByTagName('body')[0].appendChild(script)
-  getScript('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml-full.js')
+  getScript('https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js')
 }
 export function initMathJax () {
   init()
