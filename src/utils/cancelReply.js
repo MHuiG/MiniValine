26fae9443c4942bc2bcee9fb6a5199321a288aa2
MHuiG <616001163@qq.com>
2020-04-13 08:22:06 +0800

    D

--- src/utils/cancelReply.js
+++
@@ -1,7 +0,0 @@
-import dom from './plugins/dom'
-const cancelReply = (root) => {
-  dom.on('click', root.el.querySelector('.cancel-reply-btn'), (e) => {
-    root.reset()
-  })
-}
-module.exports = cancelReply
--- src/utils/script.js
+++
@@ -1,7 +0,0 @@
-import { ip } from '../Default'
-import getScript from './plugins/getScript'
-const script = (root) => {
-  getScript(ip)
-  getScript('https://cdn.polyfill.io/v3/polyfill.min.js?features=es6')
-}
-module.exports = script
