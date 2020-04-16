fcb17693588dd7319709bb800b4f544d07bb2dc9
MHuiG <616001163@qq.com>
2020-04-16 10:38:17 +0800

    aru

--- src/Default.js
+++ src/Default.js
@@ -1,7 +1,7 @@
 export const VERSION = '1.2.51'
 export const GravatarBaseUrl = 'https://gravatar.loli.net/avatar/'
 export const ip = 'https://api.ip.sb/jsonip?callback=getIP'
-export const EmoticonUrl = 'https://cdn.jsdelivr.net/npm/alus'
+export const EmoticonUrl = 'https://cdn.jsdelivr.net/npm/alus@latest'
 export const polyfill = 'https://cdn.polyfill.io/v3/polyfill.min.js?features=es6'
 export const i18nUrl = 'https://cdn.jsdelivr.net/gh/MiniValine/minivaline-i18n@master/'
 export const DefaultEmailHash = '9e63c80900d106cbbec5a9f4ea433a3e'
