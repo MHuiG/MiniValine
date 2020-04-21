e930a8e1d9841017a60d8b5b65dbb02e7bec622a
MHuiG <616001163@qq.com>
2020-04-21 16:55:27 +0800

    Update Default.js

--- src/Default.js
+++ src/Default.js
@@ -3,7 +3,7 @@ export const GBUrl = 'https://gravatar.loli.net/avatar/'
 export const ip = 'https://api.ip.sb/jsonip?callback=getIP'
 export const EUrl = 'https://cdn.jsdelivr.net/npm/alus@latest'
 export const pf = 'https://cdn.polyfill.io/v3/polyfill.min.js?features=es6'
-export const i18nUrl = 'https://cdn.jsdelivr.net/gh/MiniValine/minivaline-i18n@master/'
+export const i18nUrl = 'https://cdn.jsdelivr.net/gh/MiniValine/minivaline-i18n@latest/'
 export const DEHash = '9e63c80900d106cbbec5a9f4ea433a3e'
 export const MaxNL = 6
 export const PS = 6
