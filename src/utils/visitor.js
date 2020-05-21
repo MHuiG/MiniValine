8109abf8a0b3e3f1f445706b3865f1a65fa649c0
MHuiG <616001163@qq.com>
2020-05-21 10:34:06 +0800

    dump: minivaline-counter

--- src/utils/visitor.js
+++ src/utils/visitor.js
@@ -1,10 +1,8 @@
 const visitor = (root) => {
   if (root.visitor) {
-    var vispath = root.config.vispath ? root.config.vispath : root.config.pathname
     new MCounter({
       appId: root.config.appId,
-      appKey: root.config.appKey,
-      vispath: vispath || location.pathname.replace(/\/$/, '')
+      appKey: root.config.appKey
     })
   }
 }
