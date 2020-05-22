7a8c3902c4bc4b69399be790e9a58a085efa1234
MHuiG <616001163@qq.com>
2020-05-22 11:32:41 +0800

    Release:2.5.3

--- src/Default.js
+++ src/Default.js
@@ -1,4 +1,4 @@
-export const VERSION = '2.5.2'
+export const VERSION = '2.5.3'
 export const GBUrl = 'https://gravatar.loli.net/avatar/'
 export const ip = 'https://api.ip.sb/jsonip?callback=getIP'
 export const vis = 'https://cdn.jsdelivr.net/npm/minivaline-counter@1.3.1/dist/MCounter.min.js'
