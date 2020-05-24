c95dd35f8cfc4d527a1d49b21e35a2749987f239
MHuiG <616001163@qq.com>
2020-05-24 17:59:54 +0800

    Release:2.5.16

--- src/Default.js
+++ src/Default.js
@@ -1,7 +1,7 @@
-export const VERSION = '2.5.15'
+export const VERSION = '2.5.16'
 export const GBUrl = 'https://gravatar.loli.net/avatar/'
 export const ip = 'https://api.ip.sb/jsonip?callback=getIP'
-export const vis = 'https://cdn.jsdelivr.net/npm/minivaline-counter@1.3.7/dist/MCounter.min.js'
+export const vis = 'https://cdn.jsdelivr.net/npm/minivaline-counter@1.3.8/dist/MCounter.min.js'
 export const EUrl = ['https://cdn.jsdelivr.net/npm/alus@latest', 'https://cdn.jsdelivr.net/gh/MiniValine/qq@master', 'https://cdn.jsdelivr.net/gh/MiniValine/Bilibilis@master', 'https://cdn.jsdelivr.net/gh/MiniValine/tieba@master', 'https://cdn.jsdelivr.net/gh/MiniValine/twemoji@master', 'https://cdn.jsdelivr.net/gh/MiniValine/weibo@master']
 export const pf = 'https://cdn.polyfill.io/v3/polyfill.min.js?features=es6'
 export const i18nUrl = 'https://cdn.jsdelivr.net/gh/MiniValine/minivaline-i18n@latest/'
