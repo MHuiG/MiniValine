ad17098644f93c520ea241de49be7650dd4a7134
MHuiG <616001163@qq.com>
2020-10-15 13:41:28 +0800

    funny

--- src/Default.js
+++ src/Default.js
@@ -1,7 +1,7 @@
 export const VERSION = '4.2.9'
 export const MVUrl = 'https://cdn.jsdelivr.net/npm/minivaline@' + VERSION
 export const GBUrl = 'https://gravatar.loli.net/avatar/'
-export const EUrl = [MVUrl + '/imgs/alus', MVUrl + '/imgs/qq', MVUrl + '/imgs/Bilibilis', MVUrl + '/imgs/tieba', MVUrl + '/imgs/twemoji', MVUrl + '/imgs/weibo']
+export const EUrl = [MVUrl + '/imgs/alus', MVUrl + '/imgs/qq', MVUrl + '/imgs/Bilibilis', MVUrl + '/imgs/tieba', MVUrl + '/imgs/twemoji', MVUrl + '/imgs/weibo', MVUrl + '/imgs/funny']
 export const pf = 'https://cdn.polyfill.io/v3/polyfill.min.js?features=es6'
 export const i18nUrl = MVUrl + '/i18n/'
 export const DEHash = '9e63c80900d106cbbec5a9f4ea433a3e'
