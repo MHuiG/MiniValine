7de63e215197e8c37965e7b2b609e32213c0192c
MHuiG <616001163@qq.com>
2020-11-22 10:21:29 +0800

    waline: fetchCount

--- src/utils/fetch/waline/Base.js
+++ src/utils/fetch/waline/Base.js
@@ -1,6 +1,24 @@
+import ajax from '../../plugins/ajax'
 export function FetchBase (root) {
   root.fetchCount = (root) => {
-
+    const url = `${root.config.serverURLs}/comment`
+    try {
+      ajax({
+        url: url,
+        type: 'GET',
+        data: {
+          type: 'count',
+          url: root.config.pathname
+        },
+        success: function (data) {
+          // console.log(data)
+          root.el.querySelector('.count').innerHTML = data
+        }
+      })
+    } catch (e) {
+      console.log(e)
+      root.el.querySelector('.count').innerHTML = 0
+    }
   }
   root.fetchParentList = (root, pageNum, callback) => {
 
