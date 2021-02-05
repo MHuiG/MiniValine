a23420bf498724f515da30156cb88b5c08a4477e
MHuiG <616001163@qq.com>
2021-02-05 12:48:54 +0800

    Release:5.1.3

--- src/body/vcard.js
+++ src/body/vcard.js
@@ -4,6 +4,9 @@ const vcard = function (root, m) {
   if (m.get('nick')) {
     m.set('nick', m.get('nick').slice(0, 20).trim().replace(/&/g, '&amp;').replace(/\//g, '&#x2F').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;'))
   }
+  if (m.get('link')) {
+    m.set('link', m.get('link').slice(0, 20).trim().replace(/&/g, '&amp;').replace(/\//g, '&#x2F').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#x27;'))
+  }
   const Hash = m.get('mailMd5')
   const gravatarUrl = `${root.conf.avatarUrl + '/' + Hash}?s=48&d=${root.conf.avatarD}`
   if (root.conf.barrager) {
