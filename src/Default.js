fdfd50b168c6fe87c2cf7bdb2c4ba9ade066debd
MHuiG <616001163@qq.com>
2021-04-20 12:42:54 +0800

    Release:6.0.1

--- src/Default.js
+++ src/Default.js
@@ -1,4 +1,4 @@
-export const VERSION = '6.0.0'
+export const VERSION = '6.0.1'
 export const MVUrl = 'https://cdn.jsdelivr.net/npm/minivaline@' + VERSION
 export const avatarUrl = 'https://cdn.v2ex.com/gravatar'
 export const avatarD = 'robohash'
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { VERSION } from './Default'
 const MiniValine = (option) => {
+  if (typeof NexT != 'undefined') return
   if (!window.MV) {
     console.log('%c MiniValine %c v' + VERSION + ' ', 'color: white; background: #0078E7; padding:5px 0;', 'padding:4px;border:1px solid #0078E7;')
     window.MV = Object.create(null)
